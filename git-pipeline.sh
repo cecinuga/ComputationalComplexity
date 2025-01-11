5c07d664c4d2c2313f0fd62055f56a37f32df86b
cecinuga <cecinuga.tdb@gmail.com>
2025-01-11 01:58:47 +0100

    automate_readme_markplate

--- git-pipeline.sh
+++ git-pipeline.sh
@@ -4,7 +4,7 @@ args=("$@")
 GIT='git --git-dir='$PWD'/.git'
 MARKPLATE=/home/cecinuga/Scrivania/Programming/markplate
 
-alias python=${MARKPLATE}'/env/lib/python3.12'
+alias python=${MARKPLATE}'/env/lib/python3.12' 
 source ${MARKPLATE}/env/bin/activate
 
 python ${MARKPLATE}/markplate.py --temp ${MARKPLATE}/templates/computational_complexity.jinja --out ./${args[1]} --out-file README.md --cb ${MARKPLATE}/callbacks/computational_complexity.py --ex-cb-dir ./${args[1]}/algorithms --exclude [.git,.gitignore,README.md,git-pipeline.sh] -username cecinuga
