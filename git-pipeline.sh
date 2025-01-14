a6296ac451b189ff926a356762be5b30928585e8
cecinuga <cecinuga.tdb@gmail.com>
2025-01-14 11:47:40 +0100

    bubble_sort

--- git-pipeline.sh
+++ git-pipeline.sh
@@ -7,10 +7,10 @@ MARKPLATE=/home/cecinuga/Scrivania/Programming/markplate
 alias python=${MARKPLATE}'/env/lib/python3.12' 
 source ${MARKPLATE}/env/bin/activate
 
-python ${MARKPLATE}/markplate.py --temp ${MARKPLATE}/templates/computational_complexity.jinja --out ./${args[1]} --out-file README.md --cb ${MARKPLATE}/callbacks/computational_complexity.py --ex-cb-dir ./${args[1]}/algorithms --exclude [.git,.gitignore,README.md,git-pipeline.sh] -username cecinuga
+python ${MARKPLATE}/markplate.py --temp ${MARKPLATE}/templates/computational_complexity.jinja --out ./${args[0]} --out-file README.md --cb ${MARKPLATE}/callbacks/computational_complexity.py --ex-cb-dir ./${args[1]}/algorithms --exclude [.git,.gitignore,README.md,git-pipeline.sh] -username cecinuga
 
 deactivate
 $GIT add .
-$GIT commit -m ${args[0]}
+$GIT commit -m ${args[1]}
 $GIT push
 
