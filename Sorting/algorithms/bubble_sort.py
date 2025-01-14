dce5804327c53b7fddbb5e447ecb5963e2702945
cecinuga <cecinuga.tdb@gmail.com>
2025-01-14 11:46:34 +0100

    bubble_sort

---
+++ Sorting/algorithms/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(A: list[int]) -> list[int]:
+    for i in range(len(A)):
+        for j in range(len(A)-1, i, -1):
+            if A[j] < A[j-1]:
+                temp = A[j]
+                A[j] = A[j-1]
+                A[j-1] = temp
+
+    return A
+
